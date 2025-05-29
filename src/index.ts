90b4783ba5720e6188159708edb582f9e50939e7
NSWPOL\Momen.elKamri_nsp <momenelkamri@hotmail.com>
2025-05-29 11:01:14 +0200

    create two-dimensional game board with one move for player X

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-export const createBoard = () => {
+export const createBoard = (player: string = ' ') => {
+  if (player === 'X') {
+    return [
+      ['X', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+    ];
+  }
+
   return [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
